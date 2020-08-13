7a6cdde30fd1093d250e6a2bf41548514152bfe8
dyong1 <likedyong@gmail.com>
2020-08-13 22:20:14 +0900

    Solve 215.kth-largest-element-in-an-array

---
+++ 215.kth-largest-element-in-an-array.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=215 lang=javascript
+ *
+ * [215] Kth Largest Element in an Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    nums.sort((lhs, rhs) => rhs - lhs)
+    return nums[k-1]
+};
+// @lc code=end
+
